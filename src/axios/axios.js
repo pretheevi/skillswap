f11ee124765091cbf1b212fd77fbbb3a2ee09c70
pretheevi <pretheeviraj0805@gmail.com>
2026-01-08 14:02:20 +0530

    fixed bug

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:"http://localhost:8080/api",
+  baseURL:"https://skillswapserver-dyws.onrender.com/api",
 });
 
 // Add token automatically if exists
